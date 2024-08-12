7ace2f559c0980c01f6e7b98defb7e4d1eedf97b
Lhemingw <lhemingw@gmail.com>
2024-08-12 17:55:03 -0600

    Navbar, 2nd commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
 import { v4 as uuidv4 } from 'uuid';
+
+import Navbar from './components/Navbar.js';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // fix this import uuidv4 from 'uuid/v4', put top import { v4 as uuidv4 } from 'uuid'; install uuidv4 in package.json
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
@@ -8,7 +12,13 @@ const LOCAL_STORAGE_KEY = 'todoApp.todos'
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
-
+//  we need to store an object as well as a name so back inside of our app lets
+//     change area ([ 'Todo 1', 'Todo 2'])
+ //set todos 1. pass it a value for example empty area would clear all todos or you can just 
+    //use the previous value in our case our previous todos is a function that is going to give us the previous todos
+    //which allows us to change that so we could say our new todos is ging to be equal previous todos and we
+    //want to spread this over our area and add a new todo to that list so 
+//id: uuidv4(),
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     if (storedTodos) setTodos(storedTodos)
@@ -24,7 +34,7 @@ function App() {
     todo.complete = !todo.complete
     setTodos(newTodos)
   }
-
+ // functions so in our case antime our arrey of today's changes we want to save our todos
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
@@ -44,21 +54,30 @@ function App() {
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
+    
       <button onClick={handleClearTodos}>Clear Complete</button>
       <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+
+      {/* navbar */}
+      <div className="App">
+        <Router>
+         <Navbar />
+         <Routes>
+          <Route path="/"exact />
+         </Routes>
+         </Router>
+      </div>
     </>
-  )
+  ) 
+
 }
 
 export default App;
-  // functions so in our case antime our arrey of today's changes we want to save our todos
  
-    //set todos 1. pass it a value for example empty area would clear all todos or you can just 
-    //use the previous value in our case our previous todos is a function that is going to give us the previous todos
-    //which allows us to change that so we could say our new todos is ging to be equal previous todos and we
-    //want to spread this over our area and add a new todo to that list so 
-//id: uuidv4(),
+ 
    
+
+    //This is onClick={handleAddTodo} to put the to do list in 
 //     this will clear out text box
 
 //     previous todo, add our tod  and then set our to dos to that to do list but
@@ -69,9 +88,7 @@ export default App;
 //     variable & to access it we just say to do name ref current, which is what ever element we are going to be referencing witch is this input. .value is value of input
 
   
-//     we need to store an object as well as a name so back inside of our app lets
-//     change area ([ 'Todo 1', 'Todo 2'])
-//     <>
-    
-//  //This is onClick={handleAddTodo} to put the to do list in 
+//    
+    // <>
+   
 
